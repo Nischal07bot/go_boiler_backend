3be48217dec83775c25dce45f4dab5a0e77834c1
Nischal07bot <singhnischal953@gmail.com>
2026-03-03 23:03:00 +0530

    graceful shutdown and start of the server done

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
@@ -77,4 +78,38 @@ func New(cfg *config.Config, logger *zerolog.Logger, loggerService *loggerpkg.Lo
 	//start metric collection 
 	//Runtime metrics are automatically collected by New Relic Go agent, so no additional setup is needed here for basic runtime metrics.
 	return server, nil
+}
+
+func (s *Server) SetupHTTPServer(handler http.Handler) {
+	s.httpServer = &http.Server{
+		Addr:  "." + s.Config.Server.Port,
+		Handler: handler,
+		ReadTimeout: time.Duration(s.Config.Server.ReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(s.Config.Server.WriteTimeout) * time.Second,
+		IdleTimeout: time.Duration(s.Config.Server.IdleTimeout) * time.Second,
+	}
+}
+func (s *Server) Start() error {
+	if s.httpServer == nil {
+		return errors.New("HTTP server not initialized")
+	}
+	s.Logger.Info().
+	Str("port", s.Config.Server.Port).
+	Str("env", s.Config.Primary.Env).
+	Msg("Starting server")
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		return fmt.Errorf("failed to shutdown HTTP server: %w", err)
+	}
+	if err := s.DB.Close(); err != nil {
+		return fmt.Errorf("failed to close database connection: %w", err)
+	}
+	if s.Job != nil {
+		s.Job.Stop()
+	}
+	return nil
 }
