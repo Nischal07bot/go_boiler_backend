9f7990dac346e2f222709d108021db9ce3188ac3
Nischal07bot <singhnischal953@gmail.com>
2026-03-05 21:14:51 +0530

    rate limit newrelic integration event being sent to the dashboard

---
+++ backend/internal/middlerwares/rate_limit.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"github.com/Nischal07bot/go_boiler_backend/internal/server"
+)
+
+type RateLimitMiddleware struct {
+	server *server.Server
+}
+
+func NewRateLimitMiddleware(s *server.Server) *RateLimitMiddleware {
+	return &RateLimitMiddleware{
+		server: s,
+	}
+}
+
+func (r *RateLimitMiddleware) RecordRateLimitHit(endpoint string) {
+	if r.server.LoggerService != nil && r.server.LoggerService.GetApplication() != nil {
+		r.server.LoggerService.GetApplication().RecordCustomEvent("RateLimitHit", map[string]interface{}{
+			"endpoint": endpoint,
+		})
+		
+	}
+}
