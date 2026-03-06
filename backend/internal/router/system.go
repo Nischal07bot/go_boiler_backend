f8d8e69b9ace75e75a205757e8a29b656d46d430
Nischal07bot <singhnischal953@gmail.com>
2026-03-07 02:28:37 +0530

    router patch complete

---
+++ backend/internal/router/system.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"github.com/Nischal07bot/go_boiler_backend/internal/handler"
+
+	"github.com/labstack/echo/v4"
+)
+
+func registerSystemRoutes(r *echo.Echo, h *handler.Handlers) {
+
+}
